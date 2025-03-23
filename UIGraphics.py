1b941c8bb66796295c11e9d7eeb8a82094cf25cc
christine chemali <christine.chemali@laplateforme.io>
2025-03-23 22:48:00 +0100

    Add frame to display financial report aligned in the same line

--- UIGraphics.py
+++ UIGraphics.py
@@ -1,4 +1,4 @@
-import customtkinter as ctk 
+import customtkinter as ctk
 from database import Database
 from graphics import Graphics
 from tkinter import messagebox
@@ -7,49 +7,34 @@ from financialsummary import FinancialReport
 class GraphicsPage(ctk.CTkFrame):
     def __init__(self, parent, user_id, controller):
         super().__init__(parent)
-        self.db = Database
+        self.db = Database()
         self.user_id = user_id
         self.controller = controller
-        self.init_graphicui()
-
-    def init_graphicui(self):
-        ctk.CTkLabel(self, text = "Rapport Financier", font = ("Arial", 24, "bold")).pack(pady = 20)
-
-        ctk.CTkLabel(self, text = "Entrez :").pack(side = "left", padx = 5)
-        entry_frame = ctk.CTkFrame(self)
-        entry_frame.pack(side = "left", padx = 5)
-
-import customtkinter as ctk
-
-class GraphicsPage(ctk.CTkFrame):
-    def __init__(self, parent, user_id):
-        super().__init__(parent)
-        self.user_id = user_id
         self.init_ui()
 
     def init_ui(self):
         """ The graphic ui interface """
-        ctk.CTkLabel(self, text = "Entrez :").pack(side = "left", padx = 5)
+        ctk.CTkLabel(self, text="Rapport Financier", font=("Arial", 24, "bold")).pack(pady=20)
 
         entry_frame = ctk.CTkFrame(self)
-        entry_frame.pack(side = "left", padx = 5)
+        entry_frame.pack(pady=10)
+
+        ctk.CTkLabel(entry_frame, text = "Entrez :").pack(side = "left", padx = 5)
 
         self.month_entry = ctk.CTkEntry(entry_frame, placeholder_text = "Mois (1-12)")
         self.month_entry.pack(side = "left", padx = 5)
 
-        self.year_entry = ctk.CTkEntry(entry_frame, placeholder_text = "Année (à partir de 2023)")
+        self.year_entry = ctk.CTkEntry(entry_frame, placeholder_text ="Année (à partir de 2023)")
         self.year_entry.pack(side = "left", padx = 5)
 
-        ctk.CTkButton(self, text = "Afficher Rapport Financier", command = self.show_financial_report).pack(side = "left", padx = 5)
-
+        ctk.CTkButton(entry_frame, text = "Afficher le Rapport Financier", command = self.show_financial_report).pack(side = "left", padx = 5)
 
         ctk.CTkButton(self, text="Retour au compte", command = self.go_back_to_account).pack(pady = 10) 
 
     def show_financial_report(self):
-        """ To display the financial report """
         financial_report = FinancialReport(self.db, self.user_id)
         financial_report.financial_report_message(self.month_entry, self.year_entry)
-    
+
     def go_back_to_account(self):
         """ Go back to the user account """
         self.destroy()  
